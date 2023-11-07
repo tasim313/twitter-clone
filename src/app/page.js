47066c665e8adea3595648c34c5c803b5372b574
tasim313 <mostasimmahmudtasim@gmail.com>
2023-11-07 16:24:42 +0600

    create feed component and its header

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Feed from '@/components/Feed'
 import Sidebar from '@/components/Sidebar'
 import Image from 'next/image'
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Sidebar/>
 
       {/* Feed  */}
+      <Feed/>
 
       {/* Widgets */}
 
