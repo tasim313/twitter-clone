ad90f073b8abaebb6d177b52cd31cc26ab16271c
tasim313 <mostasimmahmudtasim@gmail.com>
2023-11-07 12:11:35 +0600

    first commit

---
+++ src/app/head.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Head = () =>{
+
+    return(
+        <>
+            <title>Twitter Clone</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta name="description" content="Google clone created by Next js" />
+            <link rel="icon" href="/favicon.svg" />
+        </>
+    )
+}
+
+export default Head;
