d6d9f5de70f88ffc1ef02f81f273ee3a24691ac1
tasim313 <mostasimmahmudtasim@gmail.com>
2023-11-08 16:52:16 +0600

    create news section of the widgets

---
+++ src/components/News.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const News = ({ article }) =>{
+    return(
+        <>
+         <a href={article.url} target="_blank">
+            <div className="flex items-center justify-between px-4 py-2 space-x-2 hover:bg-gray-200 transition duration-200">
+                <div className="space-y-0.5">
+                      <h6 className="text-sm font-bold">{article.title}</h6>
+                      <p className="text-xs font-medium text-gray-500">{article.source.name}</p>
+                </div>
+                <img className="rounded-xl" width="70" height="70" src={article.urlToImage} alt={article.title}/>
+            </div>
+         </a>
+        </>
+    )
+}
+
+export default News
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -19,7 +19,7 @@ import {
 const Sidebar = () =>{
     return(
         <>
-        <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+        <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
            {/* Twitter Logo */}
            <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
                 <Image width="50" height="50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/220px-Logo_of_Twitter.svg.png"/>
